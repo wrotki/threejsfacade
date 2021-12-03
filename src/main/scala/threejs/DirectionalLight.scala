ee04f46a2f4da95850c8aebe9b319fcbf7857658
Mariusz Borsa <mborsa@polyverse.com>
2021-12-03 11:08:33 -0800

    Add helper apply() method for easy object construction

--- src/main/scala/threejs/DirectionalLight.scala
+++ src/main/scala/threejs/DirectionalLight.scala
@@ -8,3 +8,15 @@ import scala.scalajs.js.annotation.JSGlobal
 class DirectionalLight(color: Int, intensity: Float) extends Light(color, intensity) {
   var target: Object3D = js.native
 }
+
+case object DirectionalLight {
+
+  def apply(color: Int, intensity: Float, target: Object3D, x: Float, y: Float, z: Float): DirectionalLight = {
+    val ret = new DirectionalLight(color, intensity)
+    ret.position.x = x
+    ret.position.y = y
+    ret.position.z = z
+    ret.target = target
+    ret
+  }
+}
