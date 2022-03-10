8a1ac543e7a3924b3b1c250ccc5ec70dfcc1244c
Mariusz Borsa <mborsa@polyverse.com>
2022-03-10 13:26:13 -0800

    Use BufferGeometryUtils from threejs npm

--- src/main/scala/threejs/utils/BufferGeometryUtils.scala
+++ src/main/scala/threejs/utils/BufferGeometryUtils.scala
@@ -1,5 +1,15 @@
 package threejs.utils
 
-class BufferGeometryUtils {
+import threejs.BufferGeometry
 
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+
+// examples/jsm/utils/BufferGeometryUtils.js
+
+@js.native
+@JSImport("three/examples/jsm/utils/BufferGeometryUtils.js", JSImport.Namespace)
+object BufferGeometryUtils extends js.Object {
+  def mergeBufferGeometries(geometries : js.Array[BufferGeometry], useGroups : Boolean): BufferGeometry = js.native
 }
