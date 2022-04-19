6ac2e00afddea5a48efde417d360a99b7d5af661
Mariusz Borsa <mborsa@polyverse.com>
2022-04-19 11:21:09 -0700

    Add side to Material

--- src/main/scala/threejs/Material.scala
+++ src/main/scala/threejs/Material.scala
@@ -6,4 +6,10 @@ import scala.scalajs.js.annotation.JSImport
 @js.native
 @JSImport("three", "Material")
 class Material() extends js.Object {
+  var side: Int = js.native
+}
+
+
+object Material {
+  val DoubleSide = 2
 }
